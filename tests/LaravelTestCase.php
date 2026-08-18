3aee81b36b2e6a443d85897779ffa489887367a0
Jordan <164892154+Jordanmuss99@users.noreply.github.com>
2026-08-18 10:30:39 +1000

    style: format standalone test bootstrap

--- tests/LaravelTestCase.php
+++ tests/LaravelTestCase.php
@@ -18,14 +18,14 @@ abstract class LaravelTestCase extends BaseTestCase
     {
         $paymenterBasePath = getenv('PAYMENTER_BASE_PATH');
         $bootstrap = $paymenterBasePath
-            ? rtrim($paymenterBasePath, '/\\') . '/bootstrap/app.php'
-            : __DIR__ . '/../../../../bootstrap/app.php';
+            ? rtrim($paymenterBasePath, '/\\').'/bootstrap/app.php'
+            : __DIR__.'/../../../../bootstrap/app.php';
 
-        if (!file_exists($bootstrap)) {
+        if (! file_exists($bootstrap)) {
             $bootstrap = '/var/www/paymenter/bootstrap/app.php';
         }
 
-        if (!file_exists($bootstrap)) {
+        if (! file_exists($bootstrap)) {
             throw new \RuntimeException('Unable to locate Paymenter bootstrap/app.php. Set PAYMENTER_BASE_PATH for standalone checkouts.');
         }
 
